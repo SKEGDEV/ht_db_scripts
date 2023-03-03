35dbc58442bde02881528d406258794112a35483
SKEGDEV <skegdeveloper@gmail.com>
2023-03-02 22:04:28 -0600

    v1.0.1

--- R_scripts.sql
+++ R_scripts.sql
@@ -1,36 +1,11 @@
 
+
 delimiter $$
-create procedure sp_update_teacher_token(
-  in document_number int,
-  in public_password varchar(300),
-  in session_token varchar(300),
-  in type_session int
+create procedure sp_get_teacher_session(
+  in dcoument_number varchar(100)
 )
 begin
-  set @t_id := ''; 
-  select @t_id := teacher_id from teacher
-  where teacher_documentNumber = document_number;
-
-  if type_session := 1 then
-    insert into session_user values(
-      default,
-      @t_id,
-      1,
-      public_password,
-      session_token
-    );
-  else if type_session := 2 then 
-    update session_user set
-    session_isActive = 1,
-    session_publicPassword = public_password,
-    session_token = session_token
-    where session_teacherId = @t_id;
-  else
-    update session_user set
-    session_isActive = 0,
-    session_publicPassword = '',
-    session_token = ''
-    where session_teacherId = @t_id;
-  end if
-
+  select teacher_sessionPassword from teacher
+  where teacher_documentNumber = document_number
+  limit 1;
 end$$
---
+++ U_scripts.sql
@@ -0,0 +1,39 @@
+
+delimiter $$
+create procedure sp_update_teacher_session(
+  in document_number varchar(100),
+  in public_password varchar(300),
+  in session_token varchar(300),
+  in type_session int
+)
+begin
+  set @t_id := ''; 
+  select @t_id := teacher_id from teacher
+  where teacher_documentNumber = document_number;
+
+  if type_session := 1 then
+    insert into session_user values(
+      default,
+      @t_id,
+      1,
+      public_password,
+      session_token
+    );
+  else if type_session := 2 then 
+    update session_user set
+    session_isActive = 1,
+    session_publicPassword = public_password,
+    session_token = session_token
+    where session_teacherId = @t_id;
+  else
+    update session_user set
+    session_isActive = 0,
+    session_publicPassword = '',
+    session_token = ''
+    where session_teacherId = @t_id;
+  end if
+
+  select concat(teacher_firstName, ' ', teacher_lastName) as name
+  from teacher where teacher_id = @t_id;
+
+end$$
